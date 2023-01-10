68a5e011ef60f26d50ff7375bfddcbc0f85d6e4c
LSJ <towo222@naver.com>
2023-01-10 17:29:50 +0900

    Add hand-made version of promise & future

--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,43 @@
 
 #include <chrono>
 
-void bindableFunction(int a, int b, std::mutex* mtx)
+// #include <future>
+#include "myFuture.hpp"
+
+void func(int a, int b, std::mutex* mtxCout)
 {
     using namespace std::chrono_literals;
     std::this_thread::sleep_for(30ms);
     {
-        std::lock_guard<std::mutex> lg(*mtx);
+        std::lock_guard<std::mutex> lg(*mtxCout);
         std::cout << "\t\t[inside task .. thread " << std::this_thread::get_id() << "]" << std::endl;
     }
 }
 
+template <typename T>
+void func_future(ReturnObjectDelivery<T>& promise, T a, T b, std::mutex* mtxCout)
+{
+    using namespace std::chrono_literals;
+    std::this_thread::sleep_for(30ms);
+    {
+        std::lock_guard<std::mutex> lg(*mtxCout);
+        std::cout << "\t\t[inside task .. thread " << std::this_thread::get_id() << "]" << std::endl;
+    }
+    T ret = a+b;
+    promise.set_value(ret);
+}
+
 int main(int argc, char** argv)
 {
-    ThreadPool tp(4);
+    constexpr int threadCount = 4;
+    constexpr int taskCount = 4;
 
-    for (int i = 0; i < 10; ++i) 
-    {
-        if (false) { // lambda version
+    ThreadPool tp(threadCount);
+
+    // lambda version
+    if (false) { 
+        for (int i = 0; i < taskCount; ++i) 
+        {
             tp.pushTask(std::function<void()> { [mtx = &(tp.mtxCout)]() { 
                 using namespace std::chrono_literals; 
                 std::this_thread::sleep_for(30ms);
@@ -28,10 +48,40 @@ int main(int argc, char** argv)
                 }
             }});
         }
-        if (true) {  // bind version
+    }
+
+    // bind version
+    if (false) {  
+        for (int i = 0; i < taskCount; ++i) 
+        {
             int a, b;
-            std::function<void()> task = std::bind(bindableFunction, a, b, &(tp.mtxCout));
+            std::function<void()> task = std::bind(func, a, b, &(tp.mtxCout));
             tp.pushTask(task);
+        }
+    }
+
+    // myFuture version
+    if (true) {
+        std::vector<ReturnObject<int>> returnObjects(taskCount);
+
+        for (int i = 0; i < taskCount; ++i) 
+        {
+            int a = 10, b = 30;
+            ReturnObjectDelivery<int> promise;
+            returnObjects[i].connectROD(&promise);
+
+            std::function<void()> task = std::bind(func_future<int>, promise, a, b, &(tp.mtxCout));
+            tp.pushTask(task);
+        }
+
+        // print myFuture version return value
+        for (int idx = 0; idx < returnObjects.size(); ++idx) 
+        {
+            int a = returnObjects[idx].get();
+            {
+                std::lock_guard<std::mutex> lg(tp.mtxCout);
+                std::cout << "task " << idx + 1 << " return value = " << a << std::endl;
+            }
         }
     }
 }
---
+++ src/myFuture.hpp
@@ -0,0 +1,64 @@
+#include <mutex>
+#include <condition_variable>
+#include <vector>
+
+template <typename T>
+class ReturnObjectDelivery;
+
+template <typename T>
+class ReturnObject;
+
+template <typename T>
+class ReturnObjectDelivery
+{
+private:
+public:
+    T returnValue;
+    std::vector<ReturnObject<T>*> pROs;
+    
+public:
+
+    void set_value(T& value) 
+    {
+        this->returnValue = value;
+        for (ReturnObject<T>* pRO : pROs)
+        {
+            if (pRO == nullptr) continue;
+            pRO->returnValue = this->returnValue;
+            pRO->returned = true;
+            pRO->cvReturnWait.notify_one();
+        }
+    }
+};
+
+
+template <typename T>
+class ReturnObject 
+{
+private:
+    std::mutex mtx;
+public:
+    T returnValue;
+    bool connected = false;
+    bool returned = false;
+    std::condition_variable cvReturnWait;
+public:
+    ReturnObject() = default;
+    ReturnObject(const ReturnObject&) = delete;
+
+
+    void connectROD(ReturnObjectDelivery<T>* _pROD) 
+    { 
+        if (_pROD == nullptr) return;
+        this->connected = true;
+        _pROD->pROs.push_back(this);
+    }
+
+    T get()
+    {
+        if (!connected) return T{};
+        std::unique_lock<std::mutex> ul(this->mtx);
+        if (!returned) cvReturnWait.wait(ul, [this]{ return this->returned; }); // if not yet returned, wait until returned
+        return returnValue;
+    }
+};
